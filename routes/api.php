95a8458f81cf81b71a86a258db48fe994415cb24
Prince Patel <prpatel0949@gmail.com>
2024-11-07 20:59:12 +0530

    setup auth middleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('login/google', [AuthController::class, 'loginWithGoogle']);
+Route::group(
+    ['middleware' => 'api.auth'],
+    function ($router) {
+        Route::get('login', [AuthController::class, 'loginWithGoogle']);
+    }
+);
